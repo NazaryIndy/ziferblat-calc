258a74227e9f1c7dd6d6ebaf3765c5ba6a3a1ac1
Galya <galya.pryadkina@gmail.com>
2020-02-24 23:54:27 +0300

    Add multiply

--- src/app/calc-page/calc-page.component.ts
+++ src/app/calc-page/calc-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 
 const FORMAT = 'HH:mm:ss';
-const BEFORE_TIME = moment('19:00:00', FORMAT);
+const CROSS_TIME = moment('19:00:00', FORMAT);
 
 @Component({
   selector: 'app-calc-page',
@@ -12,16 +12,16 @@ const BEFORE_TIME = moment('19:00:00', FORMAT);
 })
 export class CalcPageComponent implements OnInit {
 
-  isMeridian = false;
-  showSpinners = false;
+  public isMeridian = false;
+  public showSpinners = false;
 
-  myTime: Date;
-  price: number;
-  finalPrice: number;
-  diff: number;
-  today = new Date();
-  Holidays = require('date-holidays');
-  hd = new this.Holidays()
+  public cameTime: Date;
+  public price: number;
+  public finalPrice: number;
+  public minutesPast: number;
+  private today = new Date();
+  private Holidays = require('date-holidays');
+  private hd = new this.Holidays()
 
   constructor() { }
 
@@ -33,62 +33,70 @@ export class CalcPageComponent implements OnInit {
   // TODO make update when something change
   // TODO add scenario when it is after 00:00 and after 03:00 and when working time till 03:00
 
-  onInput(event): void {
-    // console.log('event', event)
-    // console.log('myTime', this.myTime)
+  // onInput(event): void {
+  //   // console.log('event', event)
+  //   // console.log('myTime', this.myTime)
+  //   this.price = this.getPrice(event);
+  //   this.minutesPast = this.getMinutesFromTime(event);
+  // }
+
+  public clear(): void {
+    this.cameTime = void 0;
+    this.finalPrice = null;
+  }
+
+  public calcPrice(event): void {
+    this.finalPrice = null;
     this.price = this.getPrice(event);
-    this.diff = this.getMinutesFromTime(event);
+    this.minutesPast = this.getMinutesFromTime(event);
   }
 
-  clear(): void {
-    this.myTime = void 0;
-  }
-
-  getMinutesFromTime(time: any): number {
+  private getMinutesFromTime(time: any): number {
     const newTime = moment(time);
     const now = moment();
 
     return now.diff(newTime, 'minutes');
   }
 
-  isWeekend(): boolean {
-    const weekday = moment(this.today).format('dddd'); // Monday ... Sunday
+  private isWeekend(): boolean {
+    const weekday = moment(this.today).format('dddd');
     return weekday === 'Sunday' || weekday === 'Saturday';
   }
 
-  isHoliday(): boolean {
+  private isHoliday(): boolean {
     return this.hd.isHoliday(new Date());
   }
 
-  getPrice(time: Date): number {
-    // console.log('GET PRICE')
-    const newTime = moment(time, FORMAT);
+  private getPrice(time: Date): number {
+    const cameTime = moment(time, FORMAT);
     const now = moment(new Date(), FORMAT);
     const diff = this.getMinutesFromTime(time);
-
-    // console.log('NOW', now)
-    // console.log('BEFORE', BEFORE_TIME)
-     // TODO add condition it is weekend or holiday
-    // console.log('isHoliday', this.isHoliday());
 
     if (this.isWeekend() || this.isHoliday()) {
       const result = +diff * 3;
       return result >= 480 ? 480 : result;
-    } else if (now.isBefore(BEFORE_TIME) && !this.isWeekend()) {
+    } else if (now.isBefore(CROSS_TIME) && !this.isWeekend()) {
       const result = +diff * 2.5;
       return result >= 480 ? 480 : result;
-    } else if (newTime.isAfter(BEFORE_TIME) && !this.isWeekend()) {
+    } else if (cameTime.isAfter(CROSS_TIME) && !this.isWeekend()) {
       const result = +diff * 3;
       return result >= 480 ? 480 : result;
     } else {
-      // let timeBefore19 = ;
-      console.log('else')
-      // TODO add condition if time before and after 19
+      const hoursCame = cameTime.hours();
+      const minutesCame = cameTime.minutes();
+      const hoursNow = now.hours();
+      const minutesNow = now.minutes();
+
+      const totalMinutesBefore = (19 - hoursCame) * 60 - minutesCame;
+      const totalMinutesAfter = (hoursNow - 19) * 60 + minutesNow;
+
+      const result = totalMinutesBefore * 2.5 + totalMinutesAfter * 3;
+      return result >= 480 ? 480 : result;
     }
   }
 
-  multiply(times: number): void {
-    this.finalPrice =  times * this.getPrice(this.myTime);
+  public multiply(times: number): void {
+    this.finalPrice = times * this.getPrice(this.cameTime);
   }
 
     // TODO add opportunity to substract the minutes
