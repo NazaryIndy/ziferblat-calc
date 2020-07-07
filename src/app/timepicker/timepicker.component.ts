6ba0c9a6c9a0fd8259099ab7a49322d3e278ed22
Galina Pryadkina <galya@MacBook-Pro-Galina.local>
2020-07-07 14:06:44 +0300

    [ref] Change timepicker component to custom one

--- src/app/timepicker/timepicker.component.ts
+++ src/app/timepicker/timepicker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
 
 @Component({
@@ -11,17 +11,12 @@ export class TimepickerComponent implements OnInit {
   @ViewChild('hours') hoursInput: ElementRef;
   @ViewChild('minutes') minutesInput: ElementRef;
 
-  @Input() public set controlInput(control: FormControl) {
-    this.control = control;
-    this.buildForm();
-  }
-
   @Output() public changed = new EventEmitter<any>();
 
   public form: FormGroup;
   public control: FormControl;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.buildForm();
@@ -36,10 +31,9 @@ export class TimepickerComponent implements OnInit {
 
   private buildForm(): void {
     this.form = this.fb.group({
-      hours: [this.getHours(this.control.value) || '00'],
-      minutes: [this.getMinutes(this.control.value) || '00']
+      hours: [''],
+      minutes: ['']
     });
-    this.form.valueChanges.subscribe(() => this.setControlValue());
   }
 
   private sendData(): void {
@@ -47,20 +41,19 @@ export class TimepickerComponent implements OnInit {
   }
 
   public checkInputHours(control: AbstractControl): void {
-    if (control.value.length === 2 && control.value !== '00') {
+    if (control.value.length === 2) {
       this.minutesInput.nativeElement.setSelectionRange(0, 0);
       this.minutesInput.nativeElement.focus();
     }
     if (control.value >= 24) {
       control.setValue(24);
-      this.form.get('minutes').setValue('00');
     }
     this.sendData();
   }
 
   public checkOnEmptyValue(control: AbstractControl): void {
     if (control.value === '') {
-      control.setValue('00');
+      control.setValue('');
     } else {
       if (control.value < 10) {
         control.setValue(`0${parseInt(control.value, 10)}`);
@@ -77,21 +70,6 @@ export class TimepickerComponent implements OnInit {
       }
     }
     this.sendData();
-  }
-
-  public setControlValue(): void {
-    const time = this.form.get('hours').value * 3600 + this.form.get('minutes').value * 60;
-    this.control.setValue(time);
-  }
-
-  private getHours(time: number): number|string {
-    const hours = Math.floor(time / 3600);
-    return hours >= 10 ? hours : `0${hours}`;
-  }
-
-  private getMinutes(time: number): number|string {
-    const minutes = time % 3600 / 60;
-    return minutes >= 10 ? minutes : `0${minutes}`;
   }
 
 }
