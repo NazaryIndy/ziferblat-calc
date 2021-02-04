29808fc89c8fbe728c37ef9933567baac4f3ec01
Galina Pryadkina <galya@MacBook-Pro-Galina.local>
2021-02-04 17:38:11 +0300

    Change price

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -2,6 +2,6 @@ import * as moment from 'moment';
 
 export const FORMAT = 'HH:mm:ss';
 export const CROSS_TIME = moment('19:00:00', FORMAT); // when price for 1 minute changes
-export const MAX_PRICE = 480; // stop check
-export const PRICE = 2.5; // price for 1 weekday minute
-export const PRICE_WEEKEND = 3; // price for 1 weekend minute
+export const MAX_PRICE = 590; // stop check
+export const PRICE = 3; // price for 1 weekday minute
+export const PRICE_WEEKEND = 3.5; // price for 1 weekend minute
